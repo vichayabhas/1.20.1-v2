5c7dcd197a5e206a8e0df95571859df02d81ddbf
vichayabhas <6633227421@student.chula.ac.th>
2024-08-08 16:21:45 +0700

    10

--- kubejs/server_scripts/example.js
+++ kubejs/server_scripts/example.js
@@ -7,62 +7,82 @@ ServerEvents.recipes(event => {
     event.remove({ id: "gtceu:distillation_tower/naquadria_waste_separation" })
     event.remove({ id: "gtceu:distillation_tower/enriched_naquadah_waste_separation" })
     event.recipes.gtceu.distillation_tower('test1')
-        .inputFluids(
-            Fluid.of('gtceu:enriched_naquadah_waste', 2000)
-        )
-        .itemOutputs(
-            '2x gtceu:small_barium_sulfide_dust'
-        )
+        .inputFluids(Fluid.of('gtceu:enriched_naquadah_waste', 2000))
+        .itemOutputs('2x gtceu:small_barium_sulfide_dust')
         .outputFluids(Fluid.of('gtceu:sulfuric_acid', 500))
         .outputFluids(Fluid.of('gtceu:enriched_naquadah_solution', 350))
         .outputFluids(Fluid.of('gtceu:naquadria_solution', 150))
-        .outputFluids(Fluid.of('minecraft:water',1500))
+        .outputFluids(Fluid.of('minecraft:water', 1500))
         .duration(300)
         .EUt(480)
     event.recipes.gtceu.distillation_tower('test2')
-        .inputFluids(
-            Fluid.of('gtceu:naquadria_waste', 2000)
-        )
-        .itemOutputs(
-            '2x gtceu:small_gallium_sulfide_dust'
-        )
+        .inputFluids(Fluid.of('gtceu:naquadria_waste', 2000))
+        .itemOutputs('2x gtceu:small_gallium_sulfide_dust')
         .outputFluids(Fluid.of('gtceu:sulfuric_acid', 500))
         .outputFluids(Fluid.of('gtceu:naquadria_solution', 350))
         .outputFluids(Fluid.of('gtceu:enriched_naquadah_solution', 150))
-        .outputFluids(Fluid.of('minecraft:water',1500))
+        .outputFluids(Fluid.of('minecraft:water', 1500))
         .duration(300)
         .EUt(480)
     event.recipes.gtceu.centrifuge('test3')
-        .inputFluids(
-            Fluid.of('gtceu:impure_enriched_naquadah_solution', 2000)
-        )
-        .itemOutputs(
-            'gtceu:trinium_sulfide_dust'
-        )
-
-
+        .inputFluids(Fluid.of('gtceu:impure_enriched_naquadah_solution', 2000))
+        .itemOutputs('gtceu:trinium_sulfide_dust')
         .outputFluids(Fluid.of('gtceu:enriched_naquadah_solution', 500))
-        .outputFluids(Fluid.of('minecraft:water',500))
-        .itemOutputs(
-            '2x gtceu:antimony_trifluoride_dust'
-        )
+        .outputFluids(Fluid.of('minecraft:water', 500))
+        .itemOutputs('2x gtceu:antimony_trifluoride_dust')
         .duration(400)
         .EUt(1920)
     event.recipes.gtceu.centrifuge('test4')
-        .inputFluids(
-            Fluid.of('gtceu:impure_naquadria_solution', 2000)
-        )
-        .itemOutputs(
-            'gtceu:indium_phosphide_dust'
-        )
-        .itemOutputs(
-            '2x gtceu:antimony_trifluoride_dust'
-        )
-
+        .inputFluids(Fluid.of('gtceu:impure_naquadria_solution', 2000))
+        .itemOutputs('gtceu:indium_phosphide_dust')
+        .itemOutputs('2x gtceu:antimony_trifluoride_dust')
         .outputFluids(Fluid.of('gtceu:naquadria_solution', 500))
-        .outputFluids(Fluid.of('minecraft:water',500))
+        .outputFluids(Fluid.of('minecraft:water', 500))
         .duration(400)
         .EUt(1920)
-        event.remove({id:'gtceu:centrifuge/impure_naquadria_solution_separation'})
-        event.remove({id:'gtceu:centrifuge/impure_enriched_naquadah_solution_separation'})
+    event.remove({ id: 'gtceu:centrifuge/impure_naquadria_solution_separation' })
+    event.remove({ id: 'gtceu:centrifuge/impure_enriched_naquadah_solution_separation' })
+    event.recipes.gtceu.large_chemical_reactor('test5')
+        .itemInputs('gtceu:sodium_dust')
+        .inputFluids('minecraft:water', 1000)
+        .circuit(1)
+        .itemOutputs('3x gtceu:sodium_hydroxide_dust')
+        .outputFluids(Fluid.of('gtceu:hydrogen', 1000))
+        .duration(200)
+        .EUt(30)
+    event.recipes.gtceu.large_chemical_reactor('test6')
+        .inputFluids(Fluid.of('gtceu:hydrogen', 2000))
+        .itemInputs('7x gtceu:sodium_sulfate_dust')
+        .itemOutputs('2x gtceu:sodium_dust')
+        .outputFluids(Fluid.of('gtceu:sulfuric_acid', 1000))
+        .duration(30)
+        .EUt(30)
+    event.remove({ id: 'gtceu:large_chemical_reactor/rhodium_sulfate_to_solution' })
+    event.remove({ id: 'gtceu:chemical_reactor/rhodium_sulfate_to_solution' })
+    event.remove({ id: 'gtceu:large_chemical_reactor/rhodium_sulfate_to_solution_l' })
+    event.recipes.gtceu.large_chemical_reactor('test7')
+        .inputFluids(Fluid.of('minecraft:water', 36000))
+        .inputFluids(Fluid.of('gtceu:rhodium_sulfate_gas', 39600))
+        .circuit(11)
+        .outputFluids(Fluid.of('gtceu:potassium', 7200))
+        .outputFluids(Fluid.of('gtceu:rhodium_sulfate', 39600))
+        .itemOutputs('4x gtceu:platinum_slag_dust')
+        .EUt(30)
+        .duration(1200)
+    //fluid_solidifier
+    event.recipes.gtceu.fluid_solidifier('test8')
+        .inputFluids(Fluid.of('gtceu:potassium', 144))
+        .notConsumable('gtceu:ingot_casting_mold')
+        .itemOutputs('kubejs:potassium_ingot')
+        .EUt(7)
+        .duration(20)
+        event.recipes.gtceu.macerator('test9')
+        .itemInputs('kubejs:potassium_ingot')
+        .itemOutputs('gtceu:potassium_dust')
+        .duration(39)
+        .EUt(4)
+    //gtceu:macerator
+    //gtceu:ingot_casting_mold
+    //kubejs:potassium_ingot
+    //gtceu:potassium_dust
 })//centrifuge
--- kubejs/startup_scripts/example.js
+++ kubejs/startup_scripts/example.js
@@ -4,3 +4,8 @@
 
 console.info('Hello, World! (Loaded startup scripts)')
 
+StartupEvents.registry('item', e => {
+    // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png
+    // If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json
+    e.create('potassium_ingot')
+})
